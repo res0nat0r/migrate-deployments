6ef3959d611994009e32788a197fce344bfa95b5
Stefhen Hovland <stefhen@rightscale.com>
2015-05-26 17:06:33 -0400

    additional code

--- README.md
+++ README.md
@@ -6,10 +6,26 @@ Requirements
 
 ---
 
+List all accounts you have access to:
+
+``bash
+code here
+```
+---
+
 Discover the deployment ID you wish to migrate:
 
 ```bash
 rsc -a <source account ID> cm15 index /api/deployments \
 | jq '.[] | .name, [.links[] | select(.rel=="self").href][0]' \
 | paste -sd"\t\n" - | sort
+```
+---
+
+Discover deployment group to publish to:
+
+```bash
+rsc cm15 index /api/account_groups | \
+jq '.[] | .name, [.links[] | select(.rel=="self").href][0]' | \
+paste -sd"\t\n" -
 ```
--- migrate-deployment.rb
+++ migrate-deployment.rb
@@ -1,58 +1,75 @@
 #!/usr/bin/env ruby
 
 require 'getoptlong'
+require 'json'
+require 'set'
+
+require 'pry'
 
 opts = GetoptLong.new(
   [ "--src", GetoptLong::REQUIRED_ARGUMENT ],
   [ "--dst", GetoptLong::REQUIRED_ARGUMENT ],
-  [ "--deployment", GetoptLong::REQUIRED_ARGUMENT ],
-  [ "--help", GetoptLong::NO_ARGUMENT]
+  [ "--group", GetoptLong::REQUIRED_ARGUMENT],
+  [ "--deployment", GetoptLong::REQUIRED_ARGUMENT ]
 )
 
+src_account, dst_account, group, deployment = nil
 help = <<-EOF
 #{$0} [options]
 
 --src        Source account ID to copy deployment from.
 --dst        Destination account ID to copy deployment to.
+--group      Group to publish ServerTemplate to: /api/account_groups/:id
 --deployment Deployment ID to copy from: /api/deployments/:id
 EOF
 
 opts.each do |opt, arg|
   case opt
   when "--src"
-    src_deployment = arg
+    src_account = arg
   when "--dst"
-    dst_deployment = arg
+    dst_account = arg
+  when "--group"
+    group = arg
   when "--deployment"
     deployment = arg
   end
 end
 
-unless ARGV.length == 3
-  puts help
-  exit 1
-end
+#TODO: add help
 
+# ----- Publish ServerTemplates ------
 
-__END__
-server_templates = `rsc cm16 show #{ARGV[0]} view=full | jq '.instances[].server_template.href' | sort | uniq`.split(/\n/)
-server_templates.map! { |i| i.gsub(/"/,'') } # remove inner quote marks
-account = `rsc cm16 show #{ARGV[0]} view=full | jq '.links.account.id'`
+# Get deployment info
+deployment = JSON.parse(`rsc -a #{src_account} cm16 show #{deployment} view=full`)
+server_templates = Set.new
 publications = []
+
+# Find unique server templates
+deployment['instances'].each do |instance|
+  if instance['server_template']['version'] == 0
+    STDERR.puts "ERROR: Cannot commit a HEAD version of a ServerTemplate."
+    STDERR.puts "Ensure all ServerTemplates have been committed."
+    exit 1
+  end
+  server_templates.add(instance['server_template']['href'])
+end
 
 STDERR.puts "Discovered unique ServerTemplates:\n\n"
 server_templates.each { |st| STDERR.puts st }; STDERR.puts "\n"
 
 server_templates.each do |st|
-  description = `rsc cm15 show #{st} | jq '.description'`
+  response = JSON.parse(`rsc cm15 show #{st}`)
+  description = response['description']
   short_description = description[0..255]
-  name = `rsc cm15 show #{st} | jq '.name'`
-  notes = "Auto imported from account: #{account}"
+  name = response['name']
+  notes = "Auto imported from account: #{src_account}"
 
-  STDERR.puts "Publishing: #{name.chomp} to group: #{ARGV[1].split('/').last} ...\n"
+  STDERR.puts "Publishing: #{name} to group: #{group.split('/').last} ..."
 
+  # Publish ServerTemplate
   cmd = ["rsc", "--xh", "Location", "cm15", "publish", "#{st}",
-    "account_group_hrefs[]=/api/account_groups/#{ARGV[1]}",
+    "account_group_hrefs[]=/api/account_groups/#{group}",
     "descriptions[short]=#{short_description}",
     "descriptions[notes]=#{notes}",
     "descriptions[long]=#{description}"]
@@ -66,3 +83,16 @@ end
 
 STDERR.puts "\nPUBLISHED:"
 publications.each { |p| puts p }
+
+
+# --- Import ServerTemplates ---
+
+publications.each do |pub|
+  STDERR.puts "Importing #{pub} to account: #{dst_account} ..."
+  `rsc --account #{dst_account} cm15 import #{pub}`
+end
+
+
+# --- Recreate new Deployment ---
+
+STDERR.puts "Creating 
