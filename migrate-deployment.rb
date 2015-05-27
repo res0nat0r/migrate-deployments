2765b23dd8e46c64c2f04d3b8a762e2214b6aaae
Stefhen Hovland <stefhen@rightscale.com>
2015-05-27 16:14:22 -0400

    wip

--- migrate-deployment.rb
+++ migrate-deployment.rb
@@ -4,7 +4,7 @@ require 'getoptlong'
 require 'json'
 require 'set'
 
-require 'pry'
+#require 'pry'
 
 opts = GetoptLong.new(
   [ "--src", GetoptLong::REQUIRED_ARGUMENT ],
@@ -14,7 +14,7 @@ opts = GetoptLong.new(
 )
 
 src_account, dst_account, group = nil
-deployment, deployment_name, deployment_description = nil
+new_deployment, deployment, deployment_name, deployment_description = nil
 help = <<-EOF
 #{$0} [options]
 
@@ -37,7 +37,7 @@ opts.each do |opt, arg|
   end
 end
 
-#TODO: add help
+#TODO: add help error message and arg parsing
 
 # ----- Publish ServerTemplates ------
 
@@ -88,10 +88,8 @@ end
 STDERR.puts "\nPUBLISHED:"
 publications.each { |p| puts p }; STDERR.puts "\n"
 
-
 # --- Import ServerTemplates ---
 
-#  `rsc --account #{dst_account} cm15 import #{pub}`
 publications.each do |pub|
   STDERR.puts "Importing #{pub} to account: #{dst_account} ..."
 
@@ -103,16 +101,52 @@ publications.each do |pub|
 end
 puts "\n"
 
-# --- Recreate new Deployment ---
+# --- Create new Deployment ---
 
 STDERR.puts "Creating new deployment: #{deployment_name} in account: #{dst_account} ..."
 
-cmd = ["rsc", "--account", "#{dst_account}", "cm15", "create",
-  "/api/deployments", "deployment[name]=#{deployment_name}",
+cmd = ["rsc", "--account", "#{dst_account}", "--xh", "Location",
+  "cm15", "create", "/api/deployments",
+  "deployment[name]=#{deployment_name}",
   "deployment[description]=#{deployment_description}"
 ]
+new_deployment = IO.popen(cmd, 'r+') { |io|
+  io.close_write
+  io.read
+}
+
+# --- Create Instances ---
+
+deployment['servers'].each do |server|
+  name             = server['next_instance']['name']
+  cloud            = server['next_instance']['links']['cloud']['href']
+  mci              = server['next_instance']['links']['computed_multi_cloud_image']['href']
+  instance_type    = server['next_instance']['links']['instance_type']['href']
+  ssh_key          = server['next_instance']['links']['ssh_key']['href']
+  server_template  = server['next_instance']['server_template']['href']
+  # subnets
+  # security_groups
+  # datacenter       = server['next_instance']['links']['datacenter']['href']
+
+  cmd = [
+    "rsc", "cm15", "create", "/api/servers",
+    "server[name]=#{name}",
+    "server[instance][multi_cloud_image_href]=#{mci}",
+    "server[instance][server_template_href]=#{server_template}",
+    "server[instance][instance_type_href]=#{instance_type}",
+    "server[instance][ssh_key_href]=#{ssh_key}",
+    "server[instance][cloud_href]=#{cloud}",
+    "server[instance][multi_cloud_image_href]=#{mci}",
+    "server[deployment_href]=#{new_deployment}"
+    #server[instance][inputs]=map
+    #server[instance][subnet_hrefs][]=[]string
+  ]
+
+  STDERR.puts "Creating #{name} ..."
+
   result = IO.popen(cmd, 'r+') { |io|
     io.close_write
     io.read
   }
-  STDERR.puts "#{result}"
+  puts "#{result}"
+end
